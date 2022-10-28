eb4613ac4f8295eb96266953e85be3417ccc1a79
Oleg Semochkin <semochkinov@mail.ru>
2022-10-28 23:02:59 +0700

    1

---
+++ blocks/scripts/index.js
@@ -0,0 +1,31 @@
+let editForm = document.querySelector('.profile-info');
+let editButton = editForm.querySelector('.profile-info__editButton');
+let userName = editForm.querySelector('.profile-info__name');
+let userJob = editForm.querySelector('.profile-info__job');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__closeIcon');
+let submitButton = popup.querySelector('.popup__submitButton');
+let nameInput = popup.querySelector('.popup__nameInput');
+let jobInput = popup.querySelector('.popup__jobInput');
+
+
+editButton.addEventListener('click', openPopup);
+
+function openPopup() {
+  popup.classList.add('popup_opened') ;
+}
+
+closeButton.addEventListener ('click', closePopup);
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+submitButton.addEventListener('click', closePopup);
+submitButton.addEventListener('click',formSubmitHandler);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;  
+}
