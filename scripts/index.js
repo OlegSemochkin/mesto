640a404eb68160953f92e8cd6c22c28024dbb724
Oleg Semochkin <semochkinov@mail.ru>
2022-10-31 00:53:50 +0700

    ПЗ 4_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 let editForm = document.querySelector('.profile-info');
-let editButton = editForm.querySelector('.profile-info__editButton');
+let editButton = editForm.querySelector('.profile-info__edit-button');
 let userName = editForm.querySelector('.profile-info__name');
 let userJob = editForm.querySelector('.profile-info__job');
 let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__closeIcon');
-let submitButton = popup.querySelector('.popup__submitButton');
-let nameInput = popup.querySelector('.popup__nameInput');
-let jobInput = popup.querySelector('.popup__jobInput');
-let form = popup.querySelector('.popup__formElement');
+let closeButton = popup.querySelector('.popup__close-icon');
+let submitButton = popup.querySelector('.popup__submit-button');
+let nameInput = popup.querySelector('.popup__name-input');
+let jobInput = popup.querySelector('.popup__job-input');
+let form = popup.querySelector('.popup__form-element');
 
 editButton.addEventListener('click', openPopup);
 
