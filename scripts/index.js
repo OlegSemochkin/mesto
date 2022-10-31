65df5daed09e2843a40b48139f53a5b64d300f3b
Oleg Semochkin <semochkinov@mail.ru>
2022-11-01 01:17:51 +0700

    ПЗ 4_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,22 @@
-let editForm = document.querySelector('.profile-info');
-let editButton = editForm.querySelector('.profile-info__edit-button');
-let userName = editForm.querySelector('.profile-info__name');
-let userJob = editForm.querySelector('.profile-info__job');
+let editForm = document.querySelector('.profile__profile-info');
+let editButton = editForm.querySelector('.profile__edit-button');
+let userName = editForm.querySelector('.profile__name');
+let userJob = editForm.querySelector('.profile__job');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-icon');
-let submitButton = popup.querySelector('.popup__submit-button');
 let nameInput = popup.querySelector('.popup__name-input');
 let jobInput = popup.querySelector('.popup__job-input');
 let form = popup.querySelector('.popup__form-element');
 
-editButton.addEventListener('click', openPopup);
-
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
 }
-
-closeButton.addEventListener('click', closePopup);
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
-
-submitButton.addEventListener('click', formSubmitHandler);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -30,11 +25,6 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-form.addEventListener('keydown', function (ent) {
-  if (ent.key == "Enter") {
-    userName.textContent = nameInput.value;
-    userJob.textContent = jobInput.value;
-    closePopup();
-  }
-}
-)
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+form.addEventListener('submit', formSubmitHandler);
