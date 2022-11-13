d0c5129c107c6033f2935b70131871d75ce370ba
Oleg Semochkin <semochkinov@mail.ru>
2022-11-13 21:19:32 +0700

    ПЗ 5-2 Устранены замечания ревьюера после первой иттерации.

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,34 @@
 //открытие/закрытие окон
-function openPopup(popupType) {
-  popupType.classList.add('popup_opened');
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-function closePopup(popupType) {
-  popupType.classList.remove('popup_opened');
-  popupType.classList.add('popup_closing');
-  setTimeout(function () { popupType.classList.remove('popup_closing'); }, 1000);
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 //редактирование профиля
 const editForm = document.querySelector('.profile__profile-info');
-const openPopupEdit = editForm.querySelector('.profile__edit-button');
+const openPopupEditButton = editForm.querySelector('.profile__edit-button');
 const userName = editForm.querySelector('.profile__name');
 const userJob = editForm.querySelector('.profile__job');
 const popupEdit = document.querySelector('.popup_edit');
-const closePopupEdit = popupEdit.querySelector('.popup__close-icon');
+const closePopupEditButton = popupEdit.querySelector('.popup__close-icon');
 const nameInput = popupEdit.querySelector('.popup__input_el_name');
 const jobInput = popupEdit.querySelector('.popup__input_el_job');
 const formEdit = popupEdit.querySelector('.popup__form-element');
 
-openPopupEdit.addEventListener('click', () => {
+openPopupEditButton.addEventListener('click', () => {
   openPopup(popupEdit);
   nameInput.value = userName.textContent;
   jobInput.value = userJob.textContent;
 });
-closePopupEdit.addEventListener('click', () => {
+closePopupEditButton.addEventListener('click', () => {
   closePopup(popupEdit);
 });
-formEdit.addEventListener('submit', formSubmitHandler);
+formEdit.addEventListener('submit', EditformSubmitHandler);
 
-function formSubmitHandler(evt) {
+function EditformSubmitHandler(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userJob.textContent = jobInput.value;
@@ -38,59 +36,32 @@ function formSubmitHandler(evt) {
 }
 
 //добавление карточки вручную
-const openPopupAdd = document.querySelector('.profile__add-button');
+const openPopupAddButton = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup_add');
-const closePopupAdd = popupAdd.querySelector('.popup__close-icon');
+const closePopupAddButton = popupAdd.querySelector('.popup__close-icon');
 const placeInput = popupAdd.querySelector('.popup__input_el_place');
 const linkInput = popupAdd.querySelector('.popup__input_el_link');
 const formAdd = popupAdd.querySelector(('.popup__form-element'));
 
-openPopupAdd.addEventListener('click', () => {
+openPopupAddButton.addEventListener('click', () => {
   openPopup(popupAdd);
 });
-closePopupAdd.addEventListener('click', () => {
+closePopupAddButton.addEventListener('click', () => {
   closePopup(popupAdd);
 });
-formAdd.addEventListener('submit', formSubmitHandlerAdd);
+formAdd.addEventListener('submit', AddformSubmitHandler);
 
-function formSubmitHandlerAdd(evt) {
+function AddformSubmitHandler(evt) {
   evt.preventDefault();
   renderCard({
     name: placeInput.value,
     link: linkInput.value
   });
-  placeInput.value = '';
-  linkInput.value = '';
+  formAdd.reset();
   closePopup(popupAdd);
 }
 
-//карточки из коробки
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+//отрисовка карт, с данными, хранящимися в массиве
 
 //создаем карточку и вносим в нее данные из массива
 const cardTemplate = document.querySelector('#card').content.querySelector('.element');
@@ -106,24 +77,29 @@ const generateCard = (dataCard) => {
   img.alt = dataCard.name;
 
   //добавляем отслеживание событий
-  const likeButton = newCard.querySelector('.element__like').addEventListener('click', function (evt) {
+  newCard.querySelector('.element__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__like_active');
   });
 
-  const delButton = newCard.querySelector('.element__del').addEventListener('click', function (evt) {
+  newCard.querySelector('.element__del').addEventListener('click', function (evt) {
     evt.target.closest('.element').remove();
   });
 
   //добавляем окно просмотра карточки
   const popupImg = document.querySelector('.popup_image');
+  const openPopupImgButton = newCard.querySelector('.element__photo');
+  const closePopupImgButton = popupImg.querySelector('.popup__close-icon');
+  const addImg = document.querySelector('.element__img');
+  const addDescription = document.querySelector('.element__description');
 
-  const openPopupImg = newCard.querySelector('.element__photo').addEventListener('click', () => {
+  openPopupImgButton.addEventListener('click', () => {
     openPopup(popupImg);
-    document.querySelector('.element__img').src = img.src;
-    document.querySelector('.element__description').textContent = place.textContent;
+    addImg.src = img.src;
+    addImg.alt = place.textContent;
+    addDescription.textContent = place.textContent;
   });
 
-  const closePopupImg = popupImg.querySelector('.popup__close-icon').addEventListener('click', () => {
+  closePopupImgButton.addEventListener('click', () => {
     closePopup(popupImg);
   });
 
@@ -136,6 +112,4 @@ const renderCard = (dataCard) => {
 }
 
 //делаем это для всех данных из массива
-initialCards.forEach((dataCard) => {
-  renderCard(dataCard);
-})
+initialCards.forEach(renderCard);
